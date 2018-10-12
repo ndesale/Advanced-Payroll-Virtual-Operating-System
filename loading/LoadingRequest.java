8ce55c927aab0a730ff6e19a835a9fa275a471cd
nayan2910 <42191708+nayan2910@users.noreply.github.com>
2018-10-12 14:21:40 +0530

    Add files via upload

---
+++ loading/LoadingRequest.java
@@ -0,0 +1,67 @@
+package loading;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LoadingRequest extends Thread
+{
+	JLabel lbl;
+	JPanel p1=new JPanel();
+	Container f;
+	boolean stop=true;
+
+	public LoadingRequest(Container fr)
+	{
+		f=fr;
+
+		lbl=new JLabel();
+
+		p1.setBounds(0,0,0,0);
+
+		f.add(p1);
+
+		p1.setLayout(null);
+
+		p1.add(lbl);
+
+		lbl.setBounds(0,0,60,35);
+
+		System.out.println("Tested 5");
+
+		lbl.setIcon(new ImageIcon("icons\\request\\r1.jpg"));
+		lbl.setToolTipText("Sending Request.....");
+	}
+	public void stable(boolean stop)
+	{
+		this.stop=stop;
+	}
+	public void hide()
+	{
+		p1.setVisible(false);
+		lbl.setVisible(false);
+	}
+	public void show()
+	{
+		p1.setVisible(true);
+	}
+	public void setBounds(int x,int y,int w,int h)
+	{
+		p1.setBounds(x,y,w,h);
+	}
+	public void run()
+	{
+		int i=1;
+		while(stop)
+		{
+			lbl.setIcon(new ImageIcon("icons\\request\\r"+i+".jpg"));
+			i++;
+			try
+			{
+				Thread.sleep(150);
+			}catch(Exception ex){}
+			
+			if(i>5)
+			i=1;
+		}
+	}
+}
---
+++ loading/LoadingRing.java
@@ -0,0 +1,61 @@
+package loading;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LoadingRing extends Thread
+{
+	JLabel lbl;
+	JPanel p1=new JPanel();
+	Container f;
+	boolean stop=true;
+
+	public LoadingRing(Container fr)
+	{
+		f=fr;
+
+		lbl=new JLabel();
+
+		p1.setBounds(600,300,100,100);
+
+		f.add(p1);
+
+		p1.setLayout(null);
+
+		p1.add(lbl);
+
+		lbl.setBounds(0,0,100,100);
+
+		System.out.println("Tested 5");
+
+		lbl.setIcon(new ImageIcon("icons\\loading0.jpg"));
+	}
+	public void stable(boolean stop)
+	{
+		this.stop=stop;
+	}
+	public void hide()
+	{
+		p1.setVisible(false);
+	}
+	public void show()
+	{
+		p1.setVisible(true);
+	}
+	public void run()
+	{
+		int i=0;
+		while(stop)
+		{
+			lbl.setIcon(new ImageIcon("icons\\loading"+i+".jpg"));
+			i++;
+			try
+			{
+				Thread.sleep(80);
+			}catch(Exception ex){}
+			
+			if(i>3)
+			i=0;
+		}
+	}
+}
